033ff24284b5720336f9653b78decdb91e5da13c
TheOsk <Osamakuri@gmail.com>
2020-10-30 21:26:15 +0200

    Start-git

---
+++ Game-Zanaga/Assets/Scrpits/PlayerLight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class PlayerLight : MonoBehaviour
+{
+    [SerializeField] float maxInnerRaduis = 2f;
+    [SerializeField] float minInnerRaduis = 0.5f;
+    [SerializeField] float maxOuterRaduis = 5f;
+    [SerializeField] float minOuterRaduis = 2f;
+    [SerializeField] float maxIntensity = 4f;
+    [SerializeField] float minIntensity = 0.4f;
+
+    [SerializeField] float currentInnerRaduis = 3f;
+    [SerializeField] float curretOuterRaduis = 4f;
+    [SerializeField] float currentIntensity = 3f;
+
+    [SerializeField] float intensityDecay = 0.1f;
+    [SerializeField] float raduisDecay = 0.1f;
+
+    Light2D playerLight;
+
+
+    void Start()
+    {
+        playerLight = GetComponentInChildren<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LightDecay();
+    }
+
+    void LightDecay()
+    {
+        playerLight.intensity = currentIntensity;
+        playerLight.pointLightOuterRadius = curretOuterRaduis;
+        playerLight.pointLightInnerRadius = currentInnerRaduis;
+        if(currentIntensity > minIntensity)
+            currentIntensity -= intensityDecay * Time.deltaTime;
+        if (currentInnerRaduis > minInnerRaduis)
+            currentInnerRaduis -= raduisDecay * Time.deltaTime;
+        if (curretOuterRaduis > minOuterRaduis)
+            curretOuterRaduis -= raduisDecay * Time.deltaTime;
+
+    }
+}
---
+++ Game-Zanaga/Assets/Scrpits/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(30f, 10f);
+
+    bool isAlive = true;
+
+
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myCollider;
+    BoxCollider2D feetCollider;
+    float gravityAtStart;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<CapsuleCollider2D>();
+        feetCollider = GetComponentInChildren<BoxCollider2D>();
+        gravityAtStart = myRigidBody.gravityScale;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAlive) { return; }
+
+        Run();
+        FlipSprite();
+        Jump();
+        Die();
+    }
+
+    void Run() {
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * movementSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+        bool playerHorzizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+    }
+
+    void Jump() {
+        if (!feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if (Input.GetButtonDown("Jump")) {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    void FlipSprite()
+    {
+        bool playerHorzizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHorzizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1);
+        }
+    }
+
+    void Die()
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
+        {
+           // myAnimator.SetTrigger("Die");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            isAlive = false;
+            //FindObjectOfType<GameSession>().ProcessPlayerDeath();
+        }
+    }
+}
